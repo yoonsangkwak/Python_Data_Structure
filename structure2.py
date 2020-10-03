3b60a0579ce005beb3c4d52dc7dfb82be7a0ccf0
yoonsangkwak <yunsang08@naver.com>
2020-10-03 11:09:02 +0900

    linked list

--- structure2.py
+++ structure2.py
@@ -12,3 +12,16 @@ node_1 = Node(3)
 node_2 = Node(5)
 node_3 = Node(7)
 tail_node = Node(11)
+
+# 노드들을 연결
+head_node.next = node_1
+node_1.next = node_2
+node_2.next = node_3
+node_3.next = tail_node
+
+# 노드 순서대로 출력
+iterator = head_node
+
+while iterator is not None:
+    print(iterator.data)
+    iterator = iterator.next
