20cf111091631b911aa4e8ca58b27cefde3af3c0
yoonsangkwak <yunsang08@naver.com>
2020-11-06 18:06:13 +0900

    doubly linked list

---
+++ structure9.py
@@ -0,0 +1,61 @@
+class Node:
+    """링크드 리스트의 노드 클래스"""
+    def __init__(self, data):
+        self.data = data  # 실제 노드가 저장하는 데이터
+        self.next = None  # 다음 노드에 대한 레퍼런스
+        
+        
+class LinkedList:
+    """더블리 링크드 리스트 클래스"""
+    def __init__(self):
+        self.head = None  # 링크드 리스트의 가장 앞 노드
+        self.tail = None  # 링크드 리스트의 가장 뒤 노드
+
+    def append(self,data):
+        """링크드 리스트 추가 연산 메소드"""
+        new_node = Node(data) # 새로운 데이터를 저장하는 노드
+
+        # 링크드 리스트가 비어 있는 경우
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:   # 링크드 리스트에 데이터가 이미 있는 경우
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def find_node_at(self, index):
+        """링크드 리스트 접근 연산 메소드. 파라미터 인덱스는 항상 있다고 가정"""
+        iterator = self.head    # 연결 리스트를 돌기 위해 필요한 노드 변수
+
+        for _ in range(index):
+            iterator = iterator.next
+        
+        return iterator
+
+    def __str__(self):
+        """링크드 리스트를 문자열로 표현해서 리턴하는 메소드"""
+        res_str = "|"
+
+        # 링크드 리스트 안에 모든 노드를 돌기 위한 변수. 일단 가장 앞 노드로 정의한다.
+        iterator = self.head
+
+        # 링크드 리스트 끝까지 돈다
+        while iterator is not None:
+            # 각 노드의 데이터를 리턴하는 문자열에 더해준다
+            res_str += f" {iterator.data} |"
+            iterator = iterator.next # 다음 노드로 넘어간다
+
+        return res_str
+
+
+# 빈 링크드 리스트 정의
+my_list = LinkedList()
+
+# 링크드 리스트에 데이터 추가
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+my_list.append(7)
+
+print(my_list)
